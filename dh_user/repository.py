b1d46a7b449a6ffaa8866c0a985f4557ac6ce096
JohnSoi <un.perso@yandex.ru>
2024-12-16 12:43:10 +0500

    Правка фильтрации списка пользователей по роли

--- dh_user/repository.py
+++ dh_user/repository.py
@@ -5,6 +5,7 @@ __author__: str = "Старков Е.П."
 
 from typing import Type, Any
 
+from dh_auth.repository import AccessDataRepository
 from dh_base.repositories import BaseRepository
 from sqlalchemy import Select
 
@@ -30,6 +31,7 @@ class UserRepository(BaseRepository):
             return query
 
         if filters.get("role_id"):
-            query = query.where(UserModel.access_data.role_id == filters.get("role_id"))
+            access_data = await AccessDataRepository().list({"role_id": filters.get("role_id")})
+            query = query.where(UserModel.id in (data.user_id for data in (access_data or [])))
 
         return query
